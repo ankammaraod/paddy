3ff3016a096b244d85a84df768ad0add0e4fbd08
Ankammarao D <99481377+ankammaraod@users.noreply.github.com>
2022-06-02 09:36:26 +0530

    made directory structure clean

--- paddy/src/miller.js
+++ paddy/src/miller.js
@@ -1,26 +1,30 @@
 /* eslint-disable no-magic-numbers */
 class Miller {
+  #quantity;
+  #wastePercent;
+  #bagCapacity;
+  #price;
   constructor(quantity, wastePercent, bagCapacity, price) {
-    this.quantity = quantity;
-    this.wastePercent = wastePercent;
-    this.bagCapacity = bagCapacity;
-    this.price = price;
+    this.#quantity = quantity;
+    this.#wastePercent = wastePercent;
+    this.#bagCapacity = bagCapacity;
+    this.#price = price;
   }
 
   gross() {
-    return this.bagsOfPaddy() * this.price;
+    return this.bagsOfPaddy() * this.#price;
   }
 
   bagsOfPaddy() {
-    return Math.round(this.quantity / this.bagCapacity);
+    return Math.round(this.#quantity / this.#bagCapacity);
   }
 
   wastage() {
-    return Math.floor(this.quantity * this.wastePercent);
+    return Math.floor(this.#quantity * this.#wastePercent);
   }
 
   quantityOfRice() {
-    return this.quantity - this.wastage();
+    return this.#quantity - this.wastage();
   }
 
   quantityOfPowder() {
