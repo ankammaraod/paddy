0e80158aea73090db2f1b0bbc9a30c41a63c3f68
Ankammarao D <99481377+ankammaraod@users.noreply.github.com>
2022-06-03 17:51:36 +0530

    Added paranthesis in anynonamus function

--- eventsNotifier/cropCareTakers.js
+++ eventsNotifier/cropCareTakers.js
@@ -3,39 +3,38 @@ const { EventNotifier } = require('./eventNotifier.js');
 const { Field } = require('./field.js');
 const fs = require('fs');
 
-const decideActions = (notifier, field, parsedContents) => {
+const decideActions = (eventNotifier, field, parsedContents) => {
   if (field.isHumidityRaised(parsedContents['humidity'])) {
-    notifier.notify('humidity-raised');
+    eventNotifier.notify('humidity-raised');
   }
   if (field.isTemperatureRaised(parsedContents['temperature'])) {
-    notifier.notify('temperature-raised');
+    eventNotifier.notify('temperature-raised');
   }
   if (field.isCropColorYellow(parsedContents['cropColor'])) {
-    notifier.notify('cropInYellowColor');
+    eventNotifier.notify('cropInYellowColor');
   }
 };
 
-const fileBasedEventGenerator = (notifier, field) => {
+const fileBasedEventGenerator = (eventNotifier, field) => {
   fs.watchFile('./events.json', (curr, prev) => {
     const content = fs.readFileSync('./events.json', 'utf-8');
     const parsedContents = JSON.parse(content);
     console.clear();
-    decideActions(notifier, field, parsedContents);
+    decideActions(eventNotifier, field, parsedContents);
   });
 };
 
 const main = () => {
   const eventNotifier = new EventNotifier();
-  const field = new Field(30, 30, 'green');
+  const field = new Field(30, 30, 'yellow');
 
-  eventNotifier.register('humidity-raised', () => field.openGateWalls);
-  eventNotifier.register('humidity-raised', () => field.startDrainageMotor);
-  eventNotifier.register('temperature-raised', () => field.closeGateWalls);
-  eventNotifier.register('temperature-raised', () => field.startMotor);
-  eventNotifier.register('cropInYellowColor', () => field.harvestCrop);
-  eventNotifier.register('cropInYellowColor', () => field.callBrokers);
-  eventNotifier.register('cropInYellowColor', () => field.callMillers);
-
+  eventNotifier.register('humidity-raised', () => field.openGateWalls());
+  eventNotifier.register('humidity-raised', () => field.startDrainageMotor());
+  eventNotifier.register('temperature-raised', () => field.closeGateWalls());
+  eventNotifier.register('temperature-raised', () => field.startMotor());
+  eventNotifier.register('cropInYellowColor', () => field.harvestCrop());
+  eventNotifier.register('cropInYellowColor', () => field.callBrokers());
+  eventNotifier.register('cropInYellowColor', () => field.callMillers());
   fileBasedEventGenerator(eventNotifier, field);
 };
 
